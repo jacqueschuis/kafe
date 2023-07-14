109940e317acca5a09382c3bebf2ebe4131591d4
Jack_work <jacquespariseau93@gmail.com>
2023-07-14 15:39:01 -0400

    added button to enter site

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,23 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gradient-to-b dark:bg-gradient-to-b from-brown-200 via-brown-100 to-brown-200 dark:from-brown-900 dark:via-brown-700 dark:to-brown-900">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gradient-to-b dark:bg-gradient-to-b from-brown-200 via-brown-100 to-brown-200 dark:from-brown-900 dark:via-orange-900 dark:to-brown-900">
       <div className="blobs flex flex-col justify-center items-center relative">
         <div className="bg-red-500 one blob"></div>
-        <div className="bg-green-400 three blob"></div>
-        <div className="bg-blue-400 two blob"></div>
+        <div className="bg-green-500 three blob"></div>
+        <div className="bg-blue-500 two blob"></div>
       </div>
-      <div className="rounded-2xl text-center flex flex-col items-center justify-center z-10 p-4">
-        <h1 className=" text-brown-900 dark:text-brown-100 font-bold text-9xl">
-          kafe
-        </h1>
-        <h2 className="text-orange-600 dark:text-orange-200 text-2xl">
-          your next brew, perfected
-        </h2>
+      <div className="rounded-2xl text-center flex flex-col gap-6 items-center justify-center z-10 p-4">
+        <div>
+          <h1 className=" text-brown-900 dark:text-brown-100 font-bold text-9xl">
+            kafe
+          </h1>
+          <h2 className="text-orange-600 dark:text-orange-200 text-2xl">
+            your next brew, perfected
+          </h2>
+        </div>
+        <button className="font-bold text-orange-800 px-8 py-2 rounded-xl bg-orange-400 hover:bg-orange-800 hover:text-orange-400 transition-all">
+          enter
+        </button>
       </div>
     </main>
   );
